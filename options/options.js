cce91116f74c9a6df4af9194c22f532d32b85b4d
Crowdbotics <team@crowdbotics.com>
2023-01-28 23:38:23 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "frylock-rewards-38553",
+  url: "https://frylock-rewards-38553.botics.co",
+  api: "https://frylock-rewards-38553.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
